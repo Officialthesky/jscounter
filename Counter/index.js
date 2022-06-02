13842cc36cb4784250c13d6f8c5dfdf2d24fd25f
Suraj kumar <surajkumaryadavvvv@gmail.com>
2022-06-02 17:40:27 +0530

    decrease and reset added

--- Counter/index.js
+++ Counter/index.js
@@ -7,5 +7,23 @@ function increase() {
   score.innerText = currentScore;
 }
 
+function decrease() {
+  currentScore = currentScore - 1;
+  var score = document.querySelector(".score");
+  score.innerText = currentScore;
+}
+
+function reset() {
+  currentScore = currentScore - currentScore;
+  var score = document.querySelector(".score");
+  score.innerText = currentScore;
+}
+
 var increaseBtn = document.querySelector(".increase-btn");
 increaseBtn.addEventListener("click", increase);
+
+var decreaseBtn = document.querySelector(".decrease-btn");
+decreaseBtn.addEventListener("click", decrease);
+
+var resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click", reset);
